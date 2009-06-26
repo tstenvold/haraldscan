676660800b81deb977d6d79308a40f14e2f0ab68
thebmxr <thebmxr@8f9f55d8-61c0-11de-85c0-7b9747e48d0e>
2009-06-26 02:22:27 +0000

    Only discover and service discovery coded. does virtually nothing at all at this point and is completely pointless don't download unless you want to contribute

---
+++ discovery.py
@@ -0,0 +1,30 @@
+# discovery.py
+# June 2009
+# Terence Stenvold <tstenvold@gmail.com>
+#
+
+import bluetooth
+import sys
+
+def discovery():
+
+    found_devices = bluetooth.discover_devices(duration=6, flush_cache=True, lookup_names=True)
+    #print statements used for debugging right now to be removed
+    print "found %d devices" % len(found_devices)
+
+    for addr, name in found_devices:
+        print "  %s - %s" % (addr, name)
+
+    return found_devices
+    
+def service_discovery(mac_addr):
+    
+    services = bluetooth.find_service(address=target)
+    
+    #print statements used for debugging right now to be removed
+    if len(services) > 0:
+        print "found %d services on %s" % (len(services), sys.argv[1])
+        return services
+    else:
+        print "no services found"
+         return 0
