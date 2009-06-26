6ee5359aa5d2ce863a318e2ef3b52c8d7ddb9aa1
thebmxr <thebmxr@8f9f55d8-61c0-11de-85c0-7b9747e48d0e>
2009-06-26 15:59:17 +0000

    Updated MACLIST and started to add device_class discovery and servie_class discovery

--- discovery.py
+++ discovery.py
@@ -28,3 +28,45 @@ def service_discovery(mac_addr):
     else:
         print "no services found"
          return 0
+
+
+def device_class(device_class):
+
+    major_classes = ( "Miscellaneous", 
+                          "Computer", 
+                          "Phone", 
+                          "LAN/Network Access point", 
+                          "Audio/Video", 
+                          "Peripheral", 
+                          "Imaging" )
+
+    major_class = (device_class >> 8) & 0xf
+
+    if major_class < 7:
+         class = major_classes[major_class]
+    else:
+         class = "Uncategorized"
+ 
+
+    return class
+
+def service_class(service class):
+
+    service_classes = ( (16, "positioning"), 
+                        (17, "networking"), 
+                        (18, "rendering"), 
+                        (19, "capturing"),
+                        (20, "object transfer"), 
+                        (21, "audio"), 
+                        (22, "telephony"), 
+                        (23, "information"))
+
+    serv_class = ""
+
+    for bitpos, classname in service_classes:
+        if device_class & (1 << (bitpos-1)):
+            serv_class += classname
+
+    return serv_class
+
+	
