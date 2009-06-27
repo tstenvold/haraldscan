01e9fab7b78a8c363849277bcc057a9b7a5c12e5
thebmxr <thebmxr@8f9f55d8-61c0-11de-85c0-7b9747e48d0e>
2009-06-27 05:11:57 +0000

    fixed up device class discovery and added proper computer and phone minor classes.

--- deviceclass.py
+++ deviceclass.py
@@ -5,47 +5,72 @@
 # Terence Stenvold <tstenvold@gmail.com>
 #
 
+#Major classes for Bluetooth
 def majordev_class(device_class):
 
-    major_classes = ( "Miscellaneous", 
-                        "Computer", 
-                        "Phone", 
-                        "LAN/Network Access point", 
-                        "Audio/Video", 
-                        "Peripheral", 
+    major_classes = ( "Miscellaneous",
+                        "Computer",
+                        "Phone",
+                        "LAN/Network Access point",
+                        "Audio/Video",
+                        "Peripheral",
                         "Imaging" )
 
     major_class = (device_class >> 8) & 0xf
 
     if major_class < 7:
-         class = major_classes[major_class]
-         #class += the return from minor class
+        dev_class = major_classes[major_class]
+
+        if major_class == 1:
+            dev_class += "|" + minordev_computer(device_class)
+        elif major_class == 2:
+            dev_class += "|" + minordev_computer(device_class)
+
     else:
-         class = "Uncategorized"
- 
-    return class
+        dev_class = "Uncategorized"
 
+    return dev_class
+
+#Minor classes for Computer
 def minordev_computer(device_class):
-    
-    minor_classes = ( "Miscellaneous", 
-                        "Desktop", 
-                        "Server", 
-                        "Laptop", 
-                        "Handheld PC/PDA (clam)", 
-                        "Palm sized PC/PDA", 
+
+    minor_classes = ( "Miscellaneous",
+                        "Desktop",
+                        "Server",
+                        "Laptop",
+                        "Handheld PC/PDA (Clam style)",
+                        "Palm sized PC/PDA",
                         "Wearable Computer(Watch sized)" )
-        #take only first 8 bits then shift right 1 bit to check class
-    
 
-def service_class(service class):
+    minor_class = ((device_class << 8) >> 9) & 0xf
 
-    service_classes = ( (16, "positioning"), 
-                        (17, "networking"), 
-                        (18, "rendering"), 
+    return minor_classes[minor_class]
+
+#Minor classes for phone
+def minordev_phone(device_class):
+
+    minor_classes = ( "Miscellaneous",
+                        "Cellular",
+                        "Cordless",
+                        "Smart Phone",
+                        "Wireless Modem or Voice Gateway",
+                        "Common  ISDN Gateway")
+
+    minor_class = ((device_class << 8) >> 9) & 0xf
+
+    return minor_classes[minor_class]
+
+
+#Services classes for bluetooth
+def service_class(service_class):
+
+    service_classes = ( (16, "positioning"),
+                        (17, "networking"),
+                        (18, "rendering"),
                         (19, "capturing"),
-                        (20, "object transfer"), 
-                        (21, "audio"), 
-                        (22, "telephony"), 
+                        (20, "object transfer"),
+                        (21, "audio"),
+                        (22, "telephony"),
                         (23, "information"))
 
     serv_class = ""
--- discovery.py
+++ discovery.py
@@ -8,27 +8,17 @@
 import bluetooth
 import sys
 
-def discovery():
+def discover():
 
     found_devices = bluetooth.discover_devices(duration=6, flush_cache=True, lookup_names=True)
-    #print statements used for debugging right now to be removed
-    print "found %d devices" % len(found_devices)
-
-    for addr, name in found_devices:
-        print "  %s - %s" % (addr, name)
 
     return found_devices
-    
-def service_discovery(mac_addr):
-    
+
+def service_discover(mac_addr):
+
     services = bluetooth.find_service(address=target)
-    
-    #print statements used for debugging right now to be removed
+
     if len(services) > 0:
-        print "found %d services on %s" % (len(services), sys.argv[1])
         return services
     else:
-        print "no services found"
-         return 0
-
-	
+        return 0
