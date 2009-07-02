1d59de1146d6acefc4da4acb238ba61e90780b34
thebmxr <thebmxr@8f9f55d8-61c0-11de-85c0-7b9747e48d0e>
2009-07-02 20:14:59 +0000

    Untested use R24

--- main.py
+++ main.py
@@ -16,48 +16,58 @@ def move(new_x, new_y):
 def clear():
   print '\033[2J'
 
+
+def init_screen():
+
+    clear()
+    print " "*35, 
+    print "Harald Scan"
+    print "#"*80
+    print "Press Ctrl-Z or Ctrl-C to Quit"
+    print "#"*80
+    print "MAC\t\t\tName\t\t\tClass\t\t\tManufacturer"
+
 def write_screen(cursor):
 
     result = haraldsql.show_dev_table(cursor)
 
-    move(0,0)
-
-    print "HaraldScan"
-    print "-"*20
-    print "Press Q to Quit"
-    print "-"*20
-    print "MAC\t\t\tName\t\t\tClass\t\t\tManufacturer"
-    print ""
-
+    move(0,6)
+    
     if result != None:
         for row in result:
             print row[1] + '\t' + row[2][:16] + '\t' + row[3][:16] + '\t\t' + row[4]
 
-#Open database and get connection and cursor
+def cleanup(connection, cursor):
+    haraldsql.drop_dev_table(cursor)
+    haraldsql.close_database(connection)
+    
+
+buildb = False
+write_file = False
+
+if buildb:
+    #For Building Database should be a command line option
+    haraldsql.refresh_maclist(connection)
+    #for k, v in status.iteritems():
+    #    print k, ': ', v
+
+
+#Calls to initialize the program
 connection = haraldsql.open_database()
 cursor = haraldsql.get_cursor(connection)
-
-
-#For Building Database should be a command line option
-haraldsql.refresh_maclist(connection)
-#for k, v in status.iteritems():
-#    print k, ': ', v
-
-#setup the device table for the lifetime of program
-haraldsql.setup_dev_table(connection)
-clear()
-
-#Discovers devices
+haraldsql.setup_dev_table(connection) 
 d = discovery.harald_discoverer()
 d.set_cursor(cursor)
-for i in range(1,3):
-    d.find_devices(lookup_names=True)
+init_screen()
 
-    while True:
-        d.process_event()
-        write_screen(cursor)
-        if d.done == True:
-            break
-#drops table at end of program life and close the database
-haraldsql.drop_dev_table(cursor)
-haraldsql.close_database(connection)
+try:
+    for i in range(1,10):
+        d.find_devices(lookup_names=True)
+
+        while True:
+            d.process_event()
+            write_screen(cursor)
+            if d.done == True:
+                break
+except (KeyboardInterrupt, SystemExit):
+    cleanup(connection, cursor)
