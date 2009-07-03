41877ce2ffdec9ddf11a106f3e37d2e9ec2b4e68
thebmxr <thebmxr@8f9f55d8-61c0-11de-85c0-7b9747e48d0e>
2009-07-03 23:23:10 +0000

    Another MAC and a move after program closes down

--- haraldscan.py
+++ haraldscan.py
@@ -14,6 +14,7 @@ import time,sys,os
 
 def cleanup(connection, cursor):
     haraldcli.clear()
+    haraldcli.move(0,0)
     haraldsql.drop_dev_table(cursor)
     haraldsql.close_database(connection)
 
