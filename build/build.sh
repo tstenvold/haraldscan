deffcfe4a521f199b13e8e9150e956f2f7f9828b
tstenvold <tstenvold@8f9f55d8-61c0-11de-85c0-7b9747e48d0e>
2009-12-04 23:00:14 +0000

    Small update to Build.sh and osx haraldscan points to proper dir

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@
 
 cxfreeze --target-dir=haraldscan-dist-$1 haraldscan.py
 cp MACLIST haraldscan-dist-$1/MACLIST
-cp README haraldscan-dist-$1/README
-cp gpl-3.0.txt haraldscan-dist-$1/gpl-3.0.txt
+cp doc/README haraldscan-dist-$1/README
+cp doc/gpl-3.0.txt haraldscan-dist-$1/gpl-3.0.txt
 tar -cvzf haraldscan-dist-$1.tar.gz haraldscan-dist-$1
 rm -R haraldscan-dist-$1
