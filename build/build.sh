cb64d54159b43458d90daf3b10e900466ce0b6e7
tstenvold <tstenvold@8f9f55d8-61c0-11de-85c0-7b9747e48d0e>
2010-01-09 04:44:52 +0000

    slight update to haraldargs and build script

--- build/build.sh
+++ build/build.sh
@@ -4,5 +4,6 @@ cxfreeze --target-dir=haraldscan-dist-$1 haraldscan.py
 cp MACLIST haraldscan-dist-$1/MACLIST
 cp doc/README haraldscan-dist-$1/README
 cp doc/gpl-3.0.txt haraldscan-dist-$1/gpl-3.0.txt
+chmod +x haraldscan-dist-$1/haraldscan
 tar -cvzf haraldscan-dist-$1.tar.gz haraldscan-dist-$1
 rm -R haraldscan-dist-$1
